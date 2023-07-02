3a713dc3b7f8decdb7560391a69cfce3cd7616e9
Elanis <elanis@hotmail.com>
2023-07-02 16:58:40 +0200

    Be explicit about what will be formed

--- src/components/StarsList/view.jsx
+++ src/components/StarsList/view.jsx
@@ -16,7 +16,7 @@ export default function StarsList({ disabled, formStar, i18n, resources, t }) {
 		<div className="stars-list">
 			<div className="stars-list-header">
 				<input type="range" min="1" max="100" step="0.1" value={percentage} onChange={onPercentageChange} />
-				<span className="stars-list-header-text">{percentage}% → {formatNumber(amount)}</span>
+				<span className="stars-list-header-text">{percentage}% → {formatNumber(amount)} {t('resources:hydrogen')}</span>
 				{(!disabled && amount >= 1) && <input type="button" value="Form protostar" className="stars-list-form" onClick={() => formStar(amount)} />}
 			</div>
 		</div>
